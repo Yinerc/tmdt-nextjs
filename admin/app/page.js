d1726f9f2fa6595d552eb34a002f25da2a7e253c
Thanh Hoang <thanhhoang2002n@gmail.com>
2026-06-23 14:48:16 +0700

    fix admin db port ssl

--- admin/app/page.js
+++ admin/app/page.js
@@ -1,13 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    <main className="home-page">
-      <div className="home-card">
-        <h1>TMDT Next.js</h1>
-        <p>Website thương mại điện tử đã được dựng lại theo cấu trúc Next.js.</p>
-        <Link href="/admin/login">Vào trang quản trị</Link>
-      </div>
-    </main>
-  );
-}
+export default function Home() {
+  redirect("/admin/login");
+} 
